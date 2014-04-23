c3eaa0161590b4f7b97d26e51c9befc352188bf1
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2014-04-23 15:09:10 +0900

    Modified to work with PIONE v0.4.0.

--- lib/pione/command/pione-webclient.rb
+++ lib/pione/command/pione-webclient.rb
@@ -6,73 +6,92 @@ module Pione
       # basic informations
       #
 
-      command_name("pione-webclient") {"front: %s" % [Global.front.uri]}
-      command_banner("`pione-webclient` is a PIONE client provides web interface.")
-      command_front Pione::Front::WebclientFront
+      define(:toplevel, true)
+      define(:name, "pione-webclient")
+      define(:desc, "`pione-webclient` is a PIONE client provides web interface.")
+      define(:front, Pione::Front::WebclientFront)
 
       #
       # options
       #
 
-      use_option :color
-      use_option :debug
-      use_option :communication_address
+      option :color
+      option :debug
+      option :communication_address
 
-      define_option(:environment) do |item|
+      option(:environment) do |item|
+        item.type  = :string
         item.short = '-e'
-        item.long = '--environment=MODE'
-        item.desc = 'set a environment name'
-        item.value = lambda {|name| name.to_sym}
+        item.long  = '--environment'
+        item.arg   = 'MODE'
+        item.desc  = 'set a environment name'
+        item.assign {|name| name.to_sym}
       end
 
-      define_option(:stand_alone) do |item|
+      option(:stand_alone) do |item|
         item.long = '--stand-alone'
         item.desc = 'turn on stand alone mode'
-        item.action = lambda {|_, _, _| Global.stand_alone = true}
+        item.process {Global.stand_alone = true}
       end
 
       #
       # command lifecycle: setup phase
       #
 
-      setup :dropins_app_key
-      setup :message_log_receiver
-      setup :running_environment
+      phase(:setup) do |seq|
+        seq << :dropins_app_key
+        seq << :message_log_receiver
+        seq << :running_environment
+      end
 
-      def setup_dropins_app_key
-        dropins_app_key_path = Global.webclient_root + "dropins-app-key.txt"
-        if dropins_app_key_path.exist?
-          Global.dropins_app_key = dropins_app_key_path.read.chomp
-        else
-          if option[:environment] == :production
-            abort("You should create Drop-ins app key file at %s" % dropins_app_key_path)
+      setup(:dropins_app_key) do |item|
+        item.desc = "Setup Drop-ins Appkey"
+        item.process do
+          dropins_app_key_path = Global.webclient_root + "dropins-app-key.txt"
+          if dropins_app_key_path.exist?
+            Global.dropins_app_key = dropins_app_key_path.read.chomp
           else
-            Global.dropins_app_key = ""
+            if option[:environment] == :production
+              abort("You should create Drop-ins app key file at %s" % dropins_app_key_path)
+            else
+              Global.dropins_app_key = ""
+            end
           end
         end
       end
 
       # Setup a message log receiver. This receiver sends message logs to
       # client's browser.
-      def setup_message_log_receiver
-        receiver = Log::WebclientMessageLogReceiver.new
-        Global.front[:message_log_receiver] = receiver
-        Global.job_queue.message_log_receiver = receiver
+      setup(:message_log_receiver) do |item|
+        item.desc = "Setup a message log receiver"
+        item.process do
+          receiver = Log::WebclientMessageLogReceiver.new
+          model[:front][:message_log_receiver] = receiver
+          Global.job_queue.message_log_receiver = receiver
+        end
       end
 
       # Setup webclient's running environment.
-      def setup_running_environment
-        Webclient::Application.set(:environment, option[:environment] || :development)
+      setup(:running_environment) do |item|
+        item.desc = "Setup the environment"
+        item.process do
+          Webclient::Application.set(:environment, model[:environment] || :development)
+        end
       end
 
       #
       # command lifecycle: execution phase
       #
 
-      execute :sinatra_application
+      phase(:execution) do |seq|
+        seq << :sinatra_application
+      end
 
-      def execute_sinatra_application
-        Webclient::Application.run!
+      execution(:sinatra_application) do |item|
+        item.desc = "Execute webclient as a sinatra application"
+        item.process do
+          Webclient::Application.run!
+        end
       end
     end
   end
--- lib/pione/front/webclient-front.rb
+++ lib/pione/front/webclient-front.rb
@@ -3,8 +3,8 @@ module Pione
     # WebclientFront is a front interface for +pione-webclient+ command.
     class WebclientFront < BasicFront
       # Create a new front.
-      def initialize
-        super(Global.webclient_front_port_range)
+      def initialize(cmd)
+        super(cmd, Global.webclient_front_port_range)
       end
     end
   end
