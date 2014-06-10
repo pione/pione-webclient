2ac6349bad7c4f4629a877a37de3e3bf711e8f4b
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2014-06-11 08:24:10 +0900

    Refactored.

--- lib/pione/webclient/application.rb
+++ lib/pione/webclient/application.rb
@@ -1,7 +1,30 @@
 module Pione
   module Webclient
+    class ApplicationUtil
+      def apply_template(name, locals)
+        template = Location[settings.views] + name + ".erb"
+        last_modified template.mtime
+        erb name, :locals => locals
+      end
+
+      def save_referer
+        session[:referer] = request.fullpath
+      end
+
+      def go_back(default_path)
+        if referer = session[:referer]
+          session[:referer] = nil
+          redirect session[:referer]
+        else
+          redirect default_path
+        end
+      end
+    end
+
     # `WebClient::Application` is a sinatra application.
     class Application < Sinatra::Base
+      include ApplicationUtil
+
       enable :sessions
 
       set :server, 'thin'
@@ -20,10 +43,11 @@ module Pione
         register Sinatra::Reloader
       end
 
+      # Go login page if the user is not logined.
       before do
         unless request.path_info == "/login"
           unless session[:email]
-            session[:referer] = request.fullpath;
+            save_referer
             redirect '/login'
           end
         end
@@ -33,16 +57,16 @@ module Pione
       # login
       #
 
+      # Show login page.
       get '/login' do
         unless session[:email]
-          template = Location[settings.views] + "login.erb"
-          last_modified template.mtime
-          erb :login
+          apply_template :login
         else
           redirect '/'
         end
       end
 
+      # Process authentications.
       post '/login' do
         user = User.new(params[:email], Global.workspace_root)
 
@@ -51,12 +75,7 @@ module Pione
           if user.auth(params[:password])
             session[:email] = params[:email]
 
-            if referer = session[:referer]
-              session[:referer] = nil
-              redirect session[:referer]
-            else
-              redirect '/'
-            end
+            go_back('/')
           else
             session[:message] = "Login failed because of no such user or bad password."
           end
@@ -68,15 +87,9 @@ module Pione
 
             # store the user informations to session
             session[:email] = params[:email]
-            session[:referer] = nil
 
             # go to previous page
-            if referer = session[:referer]
-              session[:referer] = nil
-              redirect session[:referer]
-            else
-              redirect '/'
-            end
+            go_back('/')
           else
             session[:message] = "The account exists already."
           end
@@ -85,6 +98,7 @@ module Pione
         redirect '/login'
       end
 
+      # Logout the user.
       get '/logout' do
         session[:email] = nil
         redirect '/login'
@@ -94,7 +108,7 @@ module Pione
       # main page
       #
 
-      # Show workspace page.
+      # Show workspace page. This page should be not cached.
       get '/' do
         user = User.new(session[:email], Global.workspace_root)
         jobs = user.find_jobs
@@ -116,14 +130,12 @@ module Pione
         end
       end
 
-      # Show job control page.
+      # Show a job management page.
       get '/job/:job_id' do
         user = User.new(session[:email], Global.workspace_root)
         job = Job.new(user, params[:job_id])
 
-        template = Location[settings.views] + "job.erb"
-        last_modified template.mtime
-        erb :job, :locals => {:job => job}
+        apply_template(:job, {:job => job})
       end
 
       # Delete the job and go home.
@@ -131,44 +143,36 @@ module Pione
         user = User.new(session[:email], Global.workspace_root)
         job = Job.new(user, params[:job_id])
 
-        if job.exist?
-          job.delete
-        end
+        # delete the job if it exists
+        job.delete if job.exist?
 
+        # go workspace
         redirect '/'
-      end
-
-      #
-      # event handlers
-      #
-
-      # Request a job.
-      Global.io.on("request") do |data, client|
-        Global.job_queue.request(data["job_id"], data["uploadMethod"], data["ppg"], data["files"])
-      end
-
-      # Cancel the job.
-      Global.io.on("cancel") do |data, client|
-        Global.job_queue.cancel(data["job_id"])
-      end
-
-      # finish interactive operation
-      Global.io.on("finish-interactive-operation") do |data, client|
-        Global.interactive_operation_manager.finish(data["job_id"], data)
       end
 
       #
       # upload
       #
+
       post '/upload/ppg/:job_id' do
-        if (req = Global.job_queue.find_request(params[:job_id]))
-          req.upload_ppg(params[:file][:filename], params[:file][:tempfile].path)
+        filename = params[:file][:filename]
+        filepath = params[:file][:tempfile].path
+
+        if handler = Global.job_queue.find_handler(params[:job_id])
+          handler.upload_ppg(filename, filepath)
+        else
+          return 404, "No such job found."
         end
       end
 
       post '/upload/file/:job_id' do
-        if (req = Global.job_queue.find_request(params[:job_id]))
-          req.upload_file(params[:file][:filename], params[:file][:tempfile].path)
+        filename = params[:file][:filename]
+        filepath = params[:file][:tempfile].path
+
+        if handler = Global.job_queue.find_handler(params[:job_id])
+          req.upload_file(filename, filepath)
+        else
+          return 404, "No such job found."
         end
       end
 
@@ -177,14 +181,18 @@ module Pione
       #
 
       # Send the processing result zip file of the session.
-      get '/result/:uuid/*.zip' do
-        if zip_location = Global.job_queue.result(params[:uuid])
+      get '/result/:job_id/:filename' do
+        user = User.new(session[:email], Global.workspace_root)
+        job = Job.new(user, params[:job_id])
+        location = job.result(params[:filename])
+
+        if job.exist? and location.exist?
           content_type "application/zip"
           last_modified zip_location.mtime
 
           send_file(zip_location.path.to_s)
         else
-          return 404, "no results"
+          return 404, "No such results"
         end
       end
 
@@ -225,6 +233,25 @@ module Pione
         sleep 5
         puts "!!! PIONE Webclient shutdowned !!!"
         exit!
+      end
+
+      #
+      # Websocket event handlers
+      #
+
+      # Request a job.
+      Global.io.on("request") do |data, client|
+        Global.job_queue.request(data["job_id"], data["uploadMethod"], data["ppg"], data["files"])
+      end
+
+      # Cancel the job.
+      Global.io.on("cancel") do |data, client|
+        Global.job_queue.cancel(data["job_id"])
+      end
+
+      # finish interactive operation
+      Global.io.on("finish-interactive-operation") do |data, client|
+        Global.interactive_operation_manager.finish(data["job_id"], data)
       end
     end
   end
